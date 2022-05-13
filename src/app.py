7ff7920404c7977e2c0bb2b9c46a42928f4a087e
jcamilofarfan <farfan1976@hotmail.com>
2022-05-13 15:54:07 -0500

    add function to POST

--- src/app.py
+++ src/app.py
@@ -17,5 +17,16 @@ def getTask(id):
             return jsonify({'task': task})
     return jsonify({'message': 'Task not found'})
 
+@app.route('/tasks', methods=['POST'])
+def createTask():
+    new_task = {
+        'id': request.json['id'],
+        'title': request.json['title'],
+        'description': request.json['description'],
+        'is_completed': request.json['is_completed']
+    }
+    Tasks.append(new_task)
+    return jsonify({'task': new_task}), 201
+
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
